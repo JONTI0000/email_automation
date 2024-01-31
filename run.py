70853fbef7d18bc279fab4264c768e09d7039400
Janinndu <janidul03@gmail.com>
2024-01-31 07:13:29 +0000

    commit

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from multiprocessing import Process
 from pre_run import getting_input
 from main import EmailSender
+from datetime import datetime
 
+start_time = datetime.now()
+now = start_time.strftime('%H:%M:%S')
 
 if __name__ == '__main__':
     instances = getting_input()
@@ -13,7 +16,13 @@ if __name__ == '__main__':
     for process in processes:
         process.join()
     
+    end_time = datetime.now()
+    end = end_time.strftime('%H:%M:%S')
+
+    time_difference = end_time - start_time
+    print(f"Time difference: {time_difference}")
     print("All functions completed")
+    
     
     
     
